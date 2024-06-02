ef0c47abfb87ca9e639f7790092fa1152d8db960
Meng Taing <taingmeng@gmail.com>
2024-06-02 23:57:57 +0800

    Fix cached request

--- src/app/api/questions/next/route.ts
+++ src/app/api/questions/next/route.ts
@@ -14,7 +14,12 @@ csv()
     questions = jsonObj;
   });
 
-export async function GET() {
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+// 'auto' | 'default-cache' | 'only-cache'
+// 'force-cache' | 'force-no-store' | 'default-no-store' | 'only-no-store'
+
+export async function GET(request: Request) {
   const randomIndex = Math.floor(Math.random() * questions.length);
   const data = questions[randomIndex];
   return Response.json({ data });
