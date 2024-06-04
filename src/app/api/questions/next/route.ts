c229299d443c983675fcccb84939300c28985538
Meng Taing <taingmeng@gmail.com>
2024-06-04 19:44:38 +0800

    Revalidate immediately

--- src/app/api/questions/next/route.ts
+++ src/app/api/questions/next/route.ts
@@ -14,8 +14,8 @@ csv()
     questions = jsonObj;
   });
 
-// export const dynamic = "force-dynamic";
-// export const fetchCache = "force-no-store";
+export const revalidate = 0;
+export const fetchCache = "default-no-store";
 // 'auto' | 'default-cache' | 'only-cache'
 // 'force-cache' | 'force-no-store' | 'default-no-store' | 'only-no-store'
 
