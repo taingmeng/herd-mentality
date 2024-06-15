2fbf5a2803c001f65080e8d9436829622bf5ab07
Meng Taing <taingmeng@gmail.com>
2024-06-15 22:29:54 +0800

    Force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ interface Question {
   options: string | undefined;
 }
 
-export const revalidate = 1;
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const questionPath = path.join(process.cwd(), 'src/data/questions.csv');
@@ -21,7 +21,6 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center px-4 py-24 justify-between overflow-hidden">
-      
       <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
         <p className="font-mono fixed left-0 top-0 flex w-full justify-center content-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           🐮 Herd Mentality
